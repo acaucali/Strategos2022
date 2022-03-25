33cb2d12f9427cd7cabe467f7f3816f72c3fb79e
acaucali <31599293+acaucali@users.noreply.github.com>
2022-03-25 14:43:12 -0500

    25-03-2022
    creacion dao ideas proyecto

---
+++ FUENTES/Strategos_Nueva_Version/Spring/StrategosNueva/src/main/java/com/strategos/nueva/bancoproyecto/ideas/dao/IdeasDocumentosAnexosDao.java
@@ -0,0 +1,10 @@
+package com.strategos.nueva.bancoproyecto.ideas.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.strategos.nueva.bancoproyecto.ideas.model.CriteriosEvaluacion;
+import com.strategos.nueva.bancoproyecto.ideas.model.IdeasDocumentosAnexos;
+
+public interface IdeasDocumentosAnexosDao extends JpaRepository<IdeasDocumentosAnexos, Long>{
+
+}
--- FUENTES/Strategos_Nueva_Version/Spring/StrategosNueva/src/main/java/com/strategos/nueva/bancoproyecto/strategos/model/OrganizacionesStrategos.java
+++ FUENTES/Strategos_Nueva_Version/Spring/StrategosNueva/src/main/java/com/strategos/nueva/bancoproyecto/strategos/model/OrganizacionesStrategos.java
@@ -19,17 +19,18 @@ public class OrganizacionesStrategos implements Serializable{
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long organizacionId;
 	
+	@Column(nullable=false)
 	private Long padreId;
 	
 	@Size(max=50)
-	@Column(nullable=true)
+	@Column(nullable=false)
 	private String nombre;
 	
-	@Size(max=50)
+	@Size(max=15)
 	@Column(nullable=true)
 	private String rif;
 	
-	@Size(max=50)
+	@Size(max=150)
 	@Column(nullable=true)
 	private String direccion;
 	
@@ -41,39 +42,216 @@ public class OrganizacionesStrategos implements Serializable{
 	@Column(nullable=true)
 	private String fax;
 	
-	private Integer mesCierre;
+	@Column(nullable=true)
+	private Byte mesCierre;
 	
+	@Column(nullable=true)
 	private Date creado;
 	
+	@Column(nullable=true)
 	private Date modificado;
 	
+	@Column(nullable=true)
 	private Long creadoId;
 	
+	@Column(nullable=true)
 	private Long modificadoId;
 	
+	@Column(nullable=true)
 	private Integer alertaMinMax;
 	
+	@Column(nullable=true)
 	private Integer alertaMetaN1;
 	
+	@Column(nullable=true)
 	private Integer alertaMetaN2;
 	
+	@Column(nullable=true)
 	private Integer alertaIniciativaZv;
 	
+	@Column(nullable=true)
 	private Integer alertaIniciativaZa;
 	
-	@Size(max=50)
 	@Column(nullable=true)
 	private String enlaceParcial;
 	
-	@Size(max=50)
 	@Column(nullable=true)
 	private String subclase;
 	
+	@Column(nullable=true)
 	private Byte visible;
 	
+	@Column(nullable=true)
 	private Byte readOnly;
-	
-	
+			
+	public Long getOrganizacionId() {
+		return organizacionId;
+	}
+
+	public void setOrganizacionId(Long organizacionId) {
+		this.organizacionId = organizacionId;
+	}
+
+	public Long getPadreId() {
+		return padreId;
+	}
+
+	public void setPadreId(Long padreId) {
+		this.padreId = padreId;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getRif() {
+		return rif;
+	}
+
+	public void setRif(String rif) {
+		this.rif = rif;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getFax() {
+		return fax;
+	}
+
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
+
+	public Byte getMesCierre() {
+		return mesCierre;
+	}
+
+	public void setMesCierre(Byte mesCierre) {
+		this.mesCierre = mesCierre;
+	}
+
+	public Date getCreado() {
+		return creado;
+	}
+
+	public void setCreado(Date creado) {
+		this.creado = creado;
+	}
+
+	public Date getModificado() {
+		return modificado;
+	}
+
+	public void setModificado(Date modificado) {
+		this.modificado = modificado;
+	}
+
+	public Long getCreadoId() {
+		return creadoId;
+	}
+
+	public void setCreadoId(Long creadoId) {
+		this.creadoId = creadoId;
+	}
+
+	public Long getModificadoId() {
+		return modificadoId;
+	}
+
+	public void setModificadoId(Long modificadoId) {
+		this.modificadoId = modificadoId;
+	}
+
+	public Integer getAlertaMinMax() {
+		return alertaMinMax;
+	}
+
+	public void setAlertaMinMax(Integer alertaMinMax) {
+		this.alertaMinMax = alertaMinMax;
+	}
+
+	public Integer getAlertaMetaN1() {
+		return alertaMetaN1;
+	}
+
+	public void setAlertaMetaN1(Integer alertaMetaN1) {
+		this.alertaMetaN1 = alertaMetaN1;
+	}
+
+	public Integer getAlertaMetaN2() {
+		return alertaMetaN2;
+	}
+
+	public void setAlertaMetaN2(Integer alertaMetaN2) {
+		this.alertaMetaN2 = alertaMetaN2;
+	}
+
+	public Integer getAlertaIniciativaZv() {
+		return alertaIniciativaZv;
+	}
+
+	public void setAlertaIniciativaZv(Integer alertaIniciativaZv) {
+		this.alertaIniciativaZv = alertaIniciativaZv;
+	}
+
+	public Integer getAlertaIniciativaZa() {
+		return alertaIniciativaZa;
+	}
+
+	public void setAlertaIniciativaZa(Integer alertaIniciativaZa) {
+		this.alertaIniciativaZa = alertaIniciativaZa;
+	}
+
+	public String getEnlaceParcial() {
+		return enlaceParcial;
+	}
+
+	public void setEnlaceParcial(String enlaceParcial) {
+		this.enlaceParcial = enlaceParcial;
+	}
+
+	public String getSubclase() {
+		return subclase;
+	}
+
+	public void setSubclase(String subclase) {
+		this.subclase = subclase;
+	}
+
+	public Byte getVisible() {
+		return visible;
+	}
+
+	public void setVisible(Byte visible) {
+		this.visible = visible;
+	}
+
+	public Byte getReadOnly() {
+		return readOnly;
+	}
+
+	public void setReadOnly(Byte readOnly) {
+		this.readOnly = readOnly;
+	}
+
 	private static final long serialVersionUID = 1L;
 			
 }
