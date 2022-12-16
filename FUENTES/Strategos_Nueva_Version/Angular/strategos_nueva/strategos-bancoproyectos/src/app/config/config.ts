2182b258aa6b9ea5ebc62db6fb0459c20ed41a1e
acaucali <31599293+acaucali@users.noreply.github.com>
2022-12-16 10:09:41 -0500

    16-12-2022

--- FUENTES/Strategos_Nueva_Version/Angular/strategos_nueva/strategos-bancoproyectos/src/app/config/config.ts
+++ FUENTES/Strategos_Nueva_Version/Angular/strategos_nueva/strategos-bancoproyectos/src/app/config/config.ts
@@ -1,31 +1,8 @@
-
-
-export const URL_BACKEND = 'http://localhost:9090';
-export const URL_FRONT = 'http://localhost:4200/';
-export const ORGANIZACION_ID = 5436;//52
-export const STRATEGOS = 'http://localhost:8095/Strategos/organizaciones/gestionarOrganizaciones.action?mostrarMisionVision=undefined';
-
-
-/*
-export const URL_BACKEND = 'http://localhost:8080/BackendBancoProyectos';
-export const URL_FRONT = 'http://localhost:4200/';
-export const ORGANIZACION_ID = 52;//52
-export const STRATEGOS = 'http://localhost:8080/Strategos2018/organizaciones/gestionarOrganizaciones.action?mostrarMisionVision=undefined';
-*/
-
-/*
-export const URL_BACKEND = 'http://localhost:8080/StrategosNueva';
-export const URL_FRONT = 'http://localhost:4200/';
-export const ORGANIZACION_ID = 5436;//52
-export const STRATEGOS = 'http://localhost:8443/Strategos/organizaciones/gestionarOrganizaciones.action?mostrarMisionVision=undefined';
-*/
-
-/*
 export const URL_BACKEND = 'https://tyr.defensoria.gov.co:8443/StrategosNueva';
 export const URL_FRONT = 'http://localhost:4200/';
 export const ORGANIZACION_ID = 148;
 export const STRATEGOS = 'https://tyr.defensoria.gov.co:8443/Strategos/organizaciones/gestionarOrganizaciones.action?mostrarMisionVision=undefined';
-*/
+
 
 export const MY_DATE_FORMATS = {
     parse: {
